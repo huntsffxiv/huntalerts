0e9f4916072089773256793357d6dff154f35ec1
Limiana <5073202+Limiana@users.noreply.github.com>
2024-01-27 23:37:25 +0300

    updating cache

--- HuntAlerts/Helpers/MessageCacheManager.cs
+++ HuntAlerts/Helpers/MessageCacheManager.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HuntAlerts.Helpers;
+#pragma warning disable IDE1006,IDE0040,IDE0044
 public class MessageCacheManager : IDisposable //IDisposable because we will need to indicate that this class is to be manually disposed, it's not required but considered good to add
 {
     DalamudLinkPayload[] PayloadList = new DalamudLinkPayload[20]; //initialize 20 cache entries
@@ -25,18 +26,18 @@ public class MessageCacheManager : IDisposable //IDisposable because we will nee
 
     public DalamudLinkPayload AddMessage(HuntTrainMessage message)
     {
-        var nextCommand = CommandCount % 20;
-        CommandCount++;
-        Messages[nextCommand] = message;
-        return PayloadList[nextCommand];
+        var nextCommand = CommandCount % 20; //real counter will reset every 20 messages
+        CommandCount++; //increase counter to indicate which slot should be used next time
+        Messages[nextCommand] = message; //store the message
+        return PayloadList[nextCommand]; //return link payload to the caller
     }
 
     void ProcessLinkPayload(uint cmd, SeString str)
     {
-        if (Messages[cmd] != null)
+        if (Messages[cmd] != null) //if message with this number exists
         {
-            HuntAlerts.P.NotifyWindow.IsOpen = true;
-            HuntAlerts.P.NotifyWindow.CurrentMessage = Messages[cmd];
+            HuntAlerts.P.NotifyWindow.IsOpen = true; //open a window
+            HuntAlerts.P.NotifyWindow.CurrentMessage = Messages[cmd]; //set window's message to it
         }
     }
 
